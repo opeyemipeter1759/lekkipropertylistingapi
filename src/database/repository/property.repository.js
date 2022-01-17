44bb365b013360a3194a4c38e598e4921c4bf11b
opeyemipeter1759 <opeyemiadebowale1759@gmail.com>
2022-01-17 02:55:16 +0100

    add not found error for property update

--- src/database/repository/property.repository.js
+++ src/database/repository/property.repository.js
@@ -1,68 +1,56 @@
-const { merge } = require( "lodash" );
-const { isValidObjectId } = require( "mongoose" );
-const utils = require( "../../shared/utils" );
-const Property = require( "../model/property.model" );
+const { merge } = require('lodash')
+const { isValidObjectId } = require('mongoose')
+const utils = require('../../shared/utils')
+const Property = require('../model/property.model')
 
 const PropertyRepository = {
-    async createProperty( payload )
-    {
-        return Property.create( payload );
-    },
-    async getPropertiesPaginated( pagination )
-    {
-        const query = { deleted: { $ne: true } };
-        const { all, page, limit, validFrom, validTo, ...otherFilters } = pagination;
-        if ( validFrom )
-        {
-            query.validFrom = { $gte: validFrom };
-        }
-        if ( validTo )
-        {
-            query.validTo = { $lte: validTo };
-        }
-        Object.keys( otherFilters ).forEach( key =>
-        {
-            const value = otherFilters[key];
-            if ( +value )
-            {
-                query[key] = +value;
-                return;
-            }
-
-            query[key] = { $regex: value, $options: 'gi' };
-        } );
-
-
-        return Property.paginate(
-            query,
-            utils.getPaginateOptions( { page, all, limit }, { createdAt: -1 } )
-        );
-    },
-    async getSingleProperty( propertyId )
-    {
-        if ( !isValidObjectId( propertyId ) )
-        {
-            return null;
-        }
-
-        return Property.findOne( { _id: propertyId } );
-    },
-    async updateSingleProperty( propertyId, update )
-    {
-        await Property.updateOne( { _id: propertyId }, { $set: update } );
-
-        return this.getSingleProperty( propertyId );
-    },
-    async updatePropertyDocument( property, update )
-    {
-        merge( property, update );
-
-        return property.save();
-    },
-    async deletePropertyDocument( property )
-    {
-        return this.updatePropertyDocument( property, { deleted: true } );
+  async createProperty(payload) {
+    return Property.create(payload)
+  },
+  async getPropertiesPaginated(pagination) {
+    const query = { deleted: { $ne: true } }
+    const { all, page, limit, validFrom, validTo, ...otherFilters } = pagination
+    if (validFrom) {
+      query.validFrom = { $gte: validFrom }
     }
-};
+    if (validTo) {
+      query.validTo = { $lte: validTo }
+    }
+    Object.keys(otherFilters).forEach((key) => {
+      const value = otherFilters[key]
+      if (+value) {
+        query[key] = +value
+        return
+      }
 
-module.exports = PropertyRepository;
+      query[key] = { $regex: value, $options: 'gi' }
+    })
+
+    return Property.paginate(
+      query,
+      utils.getPaginateOptions({ page, all, limit }, { createdAt: -1 }),
+    )
+  },
+  async getSingleProperty(propertyId) {
+    if (!isValidObjectId(propertyId)) {
+      return null
+    }
+
+    return Property.findOne({ _id: propertyId })
+  },
+  async updateSingleProperty(propertyId, update) {
+    await Property.updateOne({ _id: propertyId }, { $set: update })
+
+    return this.getSingleProperty(propertyId)
+  },
+  async updatePropertyDocument(property, update) {
+    merge(property, update)
+
+    return property.save()
+  },
+  async deletePropertyDocument(property) {
+    return this.updatePropertyDocument(property, { deleted: true })
+  },
+}
+
+module.exports = PropertyRepository
